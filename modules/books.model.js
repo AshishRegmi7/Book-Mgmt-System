a50d333c2bf2c16005a9abc8683688bb1a95e712
Ashish Regmi <ashish.reg7@gmail.com>
2024-02-07 12:31:40 +0545

    book model

--- modules/books.model.js
+++ modules/books.model.js
@@ -3,7 +3,10 @@ const {Schema ,model}=require("mongoose");
 const bookSchema=new Schema({
     title:{type:String , required:true},
     author:{type:String, required:true},
+    content:{type:String ,required:true},
     pages:{type:Number ,required:true},
+    createdAt:{type:Date,default:Date.now()},
+    updatedAt:{type:Date,default:Date.now()}
 })
 
 module.exports=new model("book",bookSchema);
